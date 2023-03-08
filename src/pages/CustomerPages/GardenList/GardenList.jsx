190d10cd7db7e2819434b4fbe32ded343935d338
Hien Giang <quachtrieuvu@gmail.com>
2023-03-08 21:27:42 +0700

    init

---
+++ src/pages/CustomerPages/GardenList/GardenList.jsx
@@ -0,0 +1,16 @@
+import classnames from 'classnames/bind'
+import styles from './GardenList.module.scss'
+import { Button, Container } from 'react-bootstrap'
+
+const cx = classnames.bind(styles);
+
+function GardenList(){
+    return(
+        <Container>
+            <h1 className={cx('garden')}>This is Garden List page</h1>
+            <Button variant="primary">Primary</Button>
+        </Container>
+    )
+}
+
+export default GardenList;
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import GardenList from "../pages/CustomerPages/GardenList/GardenList"
+
+const routes = [
+    {path: '/', component: GardenList}
+]
+
+export default routes;
