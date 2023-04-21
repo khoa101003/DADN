64caf8f58ae23535a554af13999678fffcedf8e2
NguyenXuanTrinh <trinh.nguyen2412@hcmut.edu.vn>
2023-04-22 01:02:47 +0700

    small change

--- backend/controllers/autoPump.js
+++ backend/controllers/autoPump.js
@@ -1,20 +1,20 @@
-const { default: axios } = require('axios')
-const Observable = require('./Observer')
-const userInput = require('../models/userInput.model')
+// const { default: axios } = require('axios')
+// const Observable = require('./Observer')
+// const userInput = require('../models/userInput.model')
 
 
-module.exports.logger = function (data) {
-    if(data['do-am-dat']){
-        userInput.find({})
-        .then((userInput) => {
-            if(data['do-am-dat'] < userInput[0].soil){
-                console.log("tuoi")
-            }
-            else{
-                console.log("ko tuoi")
-            }
+// module.exports.logger = function (data) {
+//     if(data['do-am-dat']){
+//         userInput.find({})
+//         .then((userInput) => {
+//             if(data['do-am-dat'] < userInput[0].soil){
+//                 console.log("tuoi")
+//             }
+//             else{
+//                 console.log("ko tuoi")
+//             }
 
-        })
-    }
-}
+//         })
+//     }
+// }
   
